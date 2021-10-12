1f24c695158bbb863180a65fb6083f77ef75687a
gnzlplcs <palaciosgm@outlook.com>
2021-10-11 22:56:22 -0500

    requirements completed + although, idk why images do not render well

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Business.css';
 
 const business = {
   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -24,15 +25,17 @@ class Business extends React.Component {
           <div className="Business-address">
             <p>{business.address}</p>
             <p>{business.city}</p>
-            <p>{business.state} {business.zipCode}</p>
+            <p>{`${business.state} ${business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating} stars</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{business.category.toUpperCase()}</h3>
+            <h3 className="rating">{`${business.rating} stars`}</h3>
+            <p>{`${business.reviewCount} reviews`}</p>
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default Business;

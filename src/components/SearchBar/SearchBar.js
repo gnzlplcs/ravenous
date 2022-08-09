f4b64f75120cabdb50b57c70d2aef75148ff9c38
gnzlplcs <palaciosgm@outlook.com>
2022-08-08 22:34:03 -0500

    add a searchbar constructor

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,24 @@
 import React from "react";
-import './SearchBar.css';
+import "./SearchBar.css";
 
 const sortByOptions = {
-  "Best Match": 'best_match',
-  "Highest Rated": 'rating',
-  "Most Reviewed": 'review_count'
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
 };
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      location: "",
+      sortBy: "best_match",
+    };
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
+    return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
@@ -19,20 +28,18 @@ class SearchBar extends React.Component {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
           <input placeholder="Search Businesses" />
           <input placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-        {/* eslint-disable-next-line */}
+          {/* eslint-disable-next-line */}
           <a>Let's Go</a>
         </div>
       </div>
-    )
+    );
   }
 }
 

9225a778c95d1b2cdd6cbfecd3cf7ac30119d4bc
gnzlplcs <palaciosgm@outlook.com>
2022-08-08 22:43:08 -0500

    get a sort option's class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count",
-};
-
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
@@ -15,11 +9,24 @@ class SearchBar extends React.Component {
       location: "",
       sortBy: "best_match",
     };
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count",
+    };
+  }
+
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return "active";
+    } else {
+      return "";
+    }
   }
 
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map((sortByOption) => {
-      let sortByOptionValue = sortByOptions[sortByOption];
+    return Object.keys(this.sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }

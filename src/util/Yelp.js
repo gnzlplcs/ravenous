835190f43ae8c6fddf64c93695242d51b3573b2d
gnzlplcs <palaciosgm@outlook.com>
2022-08-17 23:05:23 -0500

    fetch business info from Yelp

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,38 @@
-const apiKey = 'jLRvr06b4EKmYo2oFEQJCtnYihSdjwqdEXjwEgYXbku5irVaGSK5Ve7NZyJkV3jtRGzw31dNVfCiNmj8JF6NyhP_th_re7fr7a3ofwOsZLbTumYUvFvD7CYYSnVlX3Yx'
+const apiKey =
+  "jLRvr06b4EKmYo2oFEQJCtnYihSdjwqdEXjwEgYXbku5irVaGSK5Ve7NZyJkV3jtRGzw31dNVfCiNmj8JF6NyhP_th_re7fr7a3ofwOsZLbTumYUvFvD7CYYSnVlX3Yx";
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
+
+export { Yelp };
